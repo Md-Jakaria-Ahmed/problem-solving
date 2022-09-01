a13d519b81dc47aa33ca3d18cc78b7034b144fd9
Md Zakaria Ahmed <jakaria9404@gmail.com>
2022-09-01 08:43:24 +0600

    pattern right traingle with same number repeat

--- w3resource.com and codeforwin.org/right_triangle_same_number.c
+++ w3resource.com and codeforwin.org/right_triangle_same_number.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main(){
-	
+
 	int i,j,number;
 	
-	printf("Enter the number: ");
+	printf("Enter your number: ");
 	scanf("%d",&number);
 	
 	
@@ -11,14 +11,13 @@ int main(){
 		
 		for(j=1; j<=i; j++){
 			
-			printf("%d",i);
+			printf("%d",j);
 		}
 		
 		printf("\n");
-		
 	}
-	
-	
-	
+
+
+
 	return 0;
 }
